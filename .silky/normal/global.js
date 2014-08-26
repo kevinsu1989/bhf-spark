d5b35847497babca465565011f3c3aa399826ee7
Conis <wvv8oo@gmail.com>
2014-08-26 17:40:29 +0800

    项目初始化，加入vendor和silky

---
+++ .silky/normal/global.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "title": "Silky",
+    "root": "",
+    "subtitle": "这里是全局调用，通过<b>_.global.subtitle可以调用</b>",
+    "keyword": "keyword",
+    "description": "description",
+    "footer": {
+        "copyright": "HunanTV.com"
+    },
+    "linkCSS": {
+        "baseUrl": "<global.root>/css/",
+        "dir": "/css",
+        "match": /\.css|less$/,
+        path: /(less)$/,
+        to: 'css'
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+#说明
+##命名
+1. controller以文件名+Controller，directive以文件名+Directive，如mainDirective.coffee，mainDirective.coffee
+2. js一个模块一个文件夹，如果一个模块的代码比较短，可以把controller与directive整合到一个文件，这时候文件名不需要加后缀，如main.coffee
